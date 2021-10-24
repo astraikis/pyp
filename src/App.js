8f0094c9f08b87619899d0218af7629aecaec039
astraikis <astraikis.jackson@gmail.com>
2021-10-23 22:03:25 -0400

    added learn more and animations for hover

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
+import { useSelector } from 'react-redux'
 
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 import Header from "./components/Header";
 import Scanner from "./components/Scanner";
 import List from "./components/List";
+import Loader from "./components/Loader";
 
 function App() {
+  const productObject = useSelector(state => state.product);
+  const { loading } = productObject;
+
   const [showScanner, setShowScanner] = useState(true);
   const [showList, setShowList] = useState(false);
 
   return (
     <div className="App">
+      { (loading || showScanner) && <Loader /> }
       <Header />
       { showScanner && <Scanner showScanner={ showScanner } setShowScanner={ setShowScanner } setShowList={ setShowList } /> }
       { showList && <List setShowScanner={ setShowScanner } setShowList={ setShowList } /> }
--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
@@ -6,7 +6,8 @@ import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import { getProduct } from '../actions/productActions';
 
 function Scanner({ showScanner, setShowScanner, setShowList }) {
-    const [code, setCode] = React.useState("Not Found");
+    const [code, setCode] = useState("Not Found");
+    const [showMore, setShowMore] = useState(false)
 
     const dispatch = useDispatch()
     
@@ -20,7 +21,7 @@ function Scanner({ showScanner, setShowScanner, setShowList }) {
             <>
             <BarcodeScannerComponent
                 width={500}
-                height={500}
+                height={375}
                 onUpdate={(err, result) => {
                 if (result) setCode(result.text);
                 else setCode("Not Found");
@@ -28,7 +29,12 @@ function Scanner({ showScanner, setShowScanner, setShowList }) {
                 className='scanner'
             />
             </>
-            <p>scan a barcode to check for potentially harmful ingredients</p>
+            <p>scan a barcode to check for potentially harmful ingredients!</p>
+
+            <span onClick={() => setShowMore(!showMore)} className='learn-more'><p>learn more <i class="far fa-angle-double-down"></i></p></span>
+            { showMore && <div className='learn-more-text'>
+                <p>ingredients are flagged for a number of reasons including potential negative health effects, dangerous or unsustainable production techniques, and insufficient testing. i encourage you to do your own research to determine what ingredients you are comfortable with.</p>
+            </div> }
         </div>
     );
 };
